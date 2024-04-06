619cb06888e217120559ab8ff7dcc3035f0eb0c8
Kavinda Dissanayaka <92946@fhss.sjp.ac.lk>
2024-04-06 11:23:12 +0530

    Added My First Model and define attibutes

---
+++ MVC_Hunting/Models/MyFirstModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Hunting.Models
+{
+    public class MyFirstModel
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String PhoneNumber { get; set; }
+        public Boolean? WillAttend { get; set; }
+    }
+}
